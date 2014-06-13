4d35199bfea475763bd9279943a140ab22082e35
Lars Brillert <lars@railslove.com>
2014-06-13 17:39:30 +0200

    adding the name

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures backup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+name             "backup"
 %w{build-essential}.each do |cb|
   depends cb
 end
